04c5fb5bf0bdf45da5719000c5c3899b53df7988
Johnny Chen <Johnny.chen@supelec.fr>
2019-11-27 19:34:48 +0100

    svm functional

--- svm/helper.py
+++ svm/helper.py
@@ -11,6 +11,16 @@ def random_circle(number_points, noise = True, rayon = 1, x_offset = 0, y_offset
     dataframe = pd.DataFrame({'x': x, 'y': y}, index=[i for i in range(0, len(x))])
     return dataframe
 
+def random_function(function, number_points, noise = True, variance = 1, min = -2, max = 2):
+    noise = noise_normal(number_points, variance)
+    x = random_points(number_points, min, max)
+    y = function(x)
+    dataframe = pd.DataFrame({'x': x, 'y': y}, index=[i for i in range(0, len(x))])
+    return dataframe
+
+def random_sinc(number_points, noise = True, variance = 1):
+    return random_function(lambda x: np.sin(x) / x, number_points, variance)
+
 def noise_normal(number_points, variance = 1):
     return np.random.normal(0, variance, number_points)
 
--- svm/svm_linear_separable.py
+++ svm/svm_linear_separable.py
@@ -1,25 +1,33 @@
 import helper
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 
-number_of_points = 100
-first_circle = helper.random_circle(number_of_points, y_offset = 2)
+# number_of_points = 100
+# first_circle = helper.random_circle(number_of_points, y_offset = 2)
+# first_circle['classe'] = [0 for i in range(0, number_of_points)]
+# second_circle = helper.random_circle(number_of_points, x_offset = 2)
+# second_circle['classe'] = [1 for i in range(0, number_of_points)]
+# dataset = pd.concat([first_circle, second_circle])
+
+number_of_points = 1000
+first_circle = helper.random_function(lambda x: np.sin(x) / x,number_of_points, min=-10, max=10)
 first_circle['classe'] = [0 for i in range(0, number_of_points)]
-second_circle = helper.random_circle(number_of_points, x_offset = 2)
+second_circle = helper.random_function(lambda x: (np.sin(x) / x - 3), number_of_points, min=-10, max=10)
 second_circle['classe'] = [1 for i in range(0, number_of_points)]
-
-
 dataset = pd.concat([first_circle, second_circle])
+
+
 def get_color_for_classe(class_array):
-    color_bank = ['r', 'b']
+    color_bank = ['r', 'b', 'g', 'y', 'b', 'p']
     color = [color_bank[classe] for classe in class_array]
     return color
 
-# plt.plot(dataset['x'], dataset['y'], 'ro', markersize=4)
-# plt.scatter(dataset['x'], dataset['y'], c=color)
-# plt.grid()
+plt.plot(dataset['x'], dataset['y'], 'ro', markersize=4)
+plt.scatter(dataset['x'], dataset['y'], c=get_color_for_classe(dataset['classe']))
+plt.grid()
 
 x_train, x_test, y_train, y_test = train_test_split(dataset[['x', 'y']], dataset['classe'], train_size=0.8)
 clf = svm.SVC(gamma='scale')
@@ -30,14 +38,17 @@ clf.fit(x_train, y_train)
 # dual_coef = clf.dual_coef_ #les yi*alpha_i
 # decision_function = clf.decision_function
 
-x_plot = helper.random_points(1000, -5, 5)
-y_plot = helper.random_points(1000, -5, 5)
+# plot a bunch of points to see the display in plot
+x_plot = helper.random_points(1000, -10, 10)
+y_plot = helper.random_points(1000, -10, 10)
+
+x_test = pd.DataFrame({'x': x_plot, 'y': y_plot})
 
 y_predicted = clf.predict(x_test)
-mse = helper.mse(y_predicted, y_test)
+# mse = helper.mse(y_predicted, y_test)
 
 plt.scatter(x_test['x'], x_test['y'], c=get_color_for_classe(y_predicted))
-# plt.legend(mse)
+# plt.legend([mse, mse])
 plt.grid()
 
 plt.plot(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], 'go')
